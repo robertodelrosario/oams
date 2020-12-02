1d1b2b1d6960cf7b191390db3fdadd8c901eea84
roberto del rosario <roberto.delrosario@ustp.edu.ph>
2020-12-02 09:47:52 +0800

    updated instrument

--- app/Http/Controllers/API/AppliedProgramController.php
+++ app/Http/Controllers/API/AppliedProgramController.php
@@ -217,7 +217,7 @@ class AppliedProgramController extends Controller
             ->join('assigned_users', 'assigned_users.id', '=', 'instruments_scores.assigned_user_id')
             ->join('users', 'users.id', '=', 'assigned_users.user_id')
             ->where('programs_statements.program_instrument_id', $area->id)
-            ->select('programs_statements.*', 'instruments_scores.*','users.name', 'users.email' ,'assigned_users.role' )
+            ->select('programs_statements.*', 'instruments_scores.*','users.first_name','users.last_name', 'users.email' ,'assigned_users.role' )
             ->orderBy('users.id')
             ->get();
 
