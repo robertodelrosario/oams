f1d75a0bbe078e02ddf51942fcd89a32dfc1f276
roberto del rosario <roberto.delrosario@ustp.edu.ph>
2021-02-03 13:27:09 +0800

    updated acceptable gap

--- app/Http/Controllers/API/CampusController.php
+++ app/Http/Controllers/API/CampusController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Campus;
+use App\Role;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -37,6 +39,20 @@ class CampusController extends Controller
             $campus->email = $request->email;
             $campus->contact_no = $request->contact_no;
             $campus->save();
+
+            $user = new User;
+            $user->first_name = $request->first_name;
+            $user->last_name = $request->last_name;
+            $user->email = $request->email;
+            $user->contact_no = $request->contact_no;
+            $user->password = bcrypt($request->input('password'));
+            $user->status = 'active';
+            $user->save();
+            $campus= Campus::where('id',$campus->id)->first();
+            $user->campuses()->attach($campus);
+            $role = Role::where('role', 'QA director')->first();
+            $role->users()->attach($user->id);
+
             return response()->json(['status' => true, 'message' => 'Successfully created campus', 'suc' => $campus]);
         }
         return response()->json(['status' => false, 'message' => 'SUC already exist!']);
