52a049130cc75378966f8a5ddd3159dbbff5ea5f
roberto del rosario <roberto.delrosario@ustp.edu.ph>
2021-02-17 16:11:06 +0800

    updated

--- app/Http/Controllers/API/MSIEvaluationController.php
+++ app/Http/Controllers/API/MSIEvaluationController.php
@@ -9,11 +9,14 @@ use App\InstrumentParameter;
 use App\InstrumentProgram;
 use App\InstrumentScore;
 use App\ParameterMean;
+use App\Program;
 use App\Recommendation;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use Illuminate\Support\Collection;
 
 class MSIEvaluationController extends Controller
 {
@@ -55,41 +58,83 @@ class MSIEvaluationController extends Controller
     }
 
     public function showSFRData($id, $role){
-        $users = array();
         $assignedUsers = AssignedUser::where('app_program_id', $id)->get();
         if($role == 0) $role_str = 'internal accreditor';
-        if($role == 1) $role_str = 'external accreditor';
+        elseif($role == 1) $role_str = 'external accreditor';
+        $transactions = array();
         foreach ($assignedUsers as $assignedUser){
-            if(Str::contains($assignedUser->role, $role_str)) $users = Arr::prepend($users, $assignedUser);
+            $tran = DB::table('area_instruments')
+                ->join('instruments_programs', 'area_instruments.id', '=', 'instruments_programs.area_instrument_id')
+                ->where('instruments_programs.id', $assignedUser->transaction_id)
+                ->first();
+            if(!(in_array($tran, $transactions))) $transactions= Arr::prepend($transactions,$tran);
         }
+        $program = "";
         $bestpractice_array = array();
-        $remark_array = array();
+        $remark_strength_array = array();
+        $remark_weakness_array = array();
         $recommendation_array = array();
-        foreach ($users as $user){
-            $bestpractices = DB::table('assigned_users')
-                ->join('users', 'users.id', '=', 'assigned_users.user_id')
-                ->join('best_practices', 'best_practices.assigned_user_id','=', 'assigned_users.id')
-                ->where('assigned_users.id', $user->id)
-                ->get();
-            foreach ($bestpractices as $bestpractice) $bestpractice_array = Arr::prepend($bestpractice_array,$bestpractice);
+        $empty = array();
 
-            $remarks = DB::table('assigned_users')
-                ->join('users', 'users.id', '=', 'assigned_users.user_id')
-                ->join('instruments_scores', 'instruments_scores.assigned_user_id', '=', 'assigned_users.id')
-                ->where('assigned_users.id', $user->id)
-                ->where('instruments_scores.remark', '!=', null)
-                ->get();
-            foreach ($remarks as $remark) $remark_array = Arr::prepend($remark_array, $remark);
+        $collection_user = new Collection();
+        $test = array();
+        foreach ($transactions as $transaction){
+            $tasks = AssignedUser::where([
+                ['transaction_id', $transaction->id], ['app_program_id', $id]
+            ])->get();
 
-            $recommendations = DB::table('assigned_users')
-                ->join('users', 'users.id', '=', 'assigned_users.user_id')
-                ->join('recommendations', 'assigned_users.id', '=', 'recommendations.assigned_user_id')
-                ->where('assigned_users.id', $user->id)
-                ->get();
-            foreach ($recommendations as $recommendation) $recommendation_array = Arr::prepend($recommendation_array,$recommendation);
+            $area = DB::table('area_instruments')
+                ->join('instruments_programs', 'area_instruments.id','=', 'instruments_programs.area_instrument_id')
+                ->where('instruments_programs.id',$transaction->id)
+                ->first();
 
+            $program = Program::where('id', $area->program_id)->first();
+            foreach ($tasks as $task){
+                if(Str::contains($task->role, $role_str))
+                {
+                    $test= Arr::prepend($test,$task);
+                    $user = User::where('id', $task->user_id)->first();
+                    $bestpractices = DB::table('assigned_users')
+                        ->join('best_practices', 'best_practices.assigned_user_id','=', 'assigned_users.id')
+                        ->where('assigned_users.id', $task->id)
+                        ->get();
+                    foreach ($bestpractices as $bestpractice) $bestpractice_array = Arr::prepend($bestpractice_array,$bestpractice->best_practice);
+
+                    $remarks = DB::table('assigned_users')
+                        ->join('instruments_scores', 'instruments_scores.assigned_user_id', '=', 'assigned_users.id')
+                        ->where('assigned_users.id', $task->id)
+                        ->where('instruments_scores.remark', '!=', null)
+                        ->get();
+                    if(count($remarks) > 0)
+                    foreach ($remarks as $remark)
+                    {
+                        if($remark->remark_type == 'Strength') $remark_strength_array = Arr::prepend($remark_strength_array, $remark->remark);
+                        elseif($remark->remark_type == 'Weakness') $remark_weakness_array = Arr::prepend($remark_weakness_array, $remark->remark);
+                    }
+
+                    $recommendations = DB::table('assigned_users')
+                        ->join('recommendations', 'assigned_users.id', '=', 'recommendations.assigned_user_id')
+                        ->where('assigned_users.id', $task->id)
+                        ->get();
+                    foreach ($recommendations as $recommendation) $recommendation_array = Arr::prepend($recommendation_array,$recommendation->recommendation);
+
+                    $collection_user->push([
+                        'instrument_program_id' => $area->id,
+                        'area_name' => $area->area_name,
+                        'user_name' => $user->first_name. " ".$user->last_name,
+                        'best_practices' => $bestpractice_array,
+                        'strength_remarks' => $remark_strength_array,
+                        'weakness_remarks' => $remark_weakness_array,
+                        'recommendations' => $recommendation_array
+                    ]);
+                    $bestpractice_array = $empty;
+                    $remark_strength_array = $empty;
+                    $remark_weakness_array= $empty;
+                    $recommendation_array= $empty;
+                }
+            }
         }
-        return response()->json(['best_practice' => $bestpractice_array, 'remark' => $remark_array, 'recommendation' => $recommendation_array]);
+        return response()->json(['program' => $program, 'instrument_programs' => $transactions, 'collection' => $collection_user]);
     }
 
     public function deleteBestPractice($id){
