af796556dcf94469ec1d33ee117d27f7e15bc251
roberto del rosario <roberto.delrosario@ustp.edu.ph>
2021-03-15 14:53:24 +0800

    updated

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -167,6 +167,7 @@ class AuthController extends Controller
             }
             $role->users()->attach($user->id);
             $user_office = CampusUser::where('user_id', $user->id)->first();
+            dd($user_office);
             $user_office->office_id = $request->office_id;
             $user_office->save();
 
