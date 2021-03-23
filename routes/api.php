d2d347906e4db26ba9927577deb857897cf403f5
roberto del rosario <roberto.delrosario@ustp.edu.ph>
2021-03-23 15:56:36 +0800

    updated

--- routes/api.php
+++ routes/api.php
@@ -155,8 +155,10 @@ Route::group(['prefix' => '/document'], function() {
     Route::put('/editDocumentName/{id}', 'DocumentController@editDocumentName');
     Route::post('/addTag/{id}', 'DocumentController@addTag');
     Route::delete('/deleteTag/{id}', 'DocumentController@deleteTag');
+    Route::post('/makeOwnDocumentList/{id}', 'DocumentController@makeOwnDocumentList');
     Route::post('/uploadOwnDocument/{id}/{statementID}', 'DocumentController@uploadOwnDocument');
     Route::get('/showOwnDocument/{id}', 'DocumentController@showOwnDocument');
+    Route::get('/showOwnContainer/{id}', 'DocumentController@showOwnContainer');
     Route::delete('/deleteAllDocument', 'DocumentController@deleteAllDocument');
 });
 
