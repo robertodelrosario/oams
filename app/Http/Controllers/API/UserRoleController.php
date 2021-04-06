580e729f86574528df50805456d03c30e80e366d
roberto del rosario <roberto.delrosario@ustp.edu.ph>
2021-04-06 09:42:59 +0800

    updated

--- app/Http/Controllers/API/UserRoleController.php
+++ app/Http/Controllers/API/UserRoleController.php
@@ -18,6 +18,7 @@ class UserRoleController extends Controller
     public function setRole(request $request,$userID){
         $role = Role::where('role', $request->role)->first();
         $users = OfficeUser::where('office_id',  $request->office_id)->get();
+        dd(count($users));
         if(count($users) > 0) {
             if ($role->id == 3) {
                 $users = OfficeUser::where('office_id', $request->office_id)->get();
