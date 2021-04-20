b87e7cf74cb1f0cf76f1287a2fc09235e733e47a
roberto del rosario <roberto.delrosario@ustp.edu.ph>
2021-04-20 16:22:14 +0800

    updated

--- routes/api.php
+++ routes/api.php
@@ -204,7 +204,7 @@ Route::group(['prefix' => '/instrument'], function() {
     Route::post('/createParameter', 'ParameterController@createParameter');
     Route::get('/showProgram', 'InstrumentController@showProgram');
     Route::get('/showParameter/{id}', 'ParameterController@showParameter');
-    Route::get('/showInstrument/{id}', 'InstrumentController@showInstrument');
+    Route::get('/showInstrument/{id}', 'StatementController@showInstrument');
     Route::get('/showAllStatement', 'InstrumentController@showAllStatement');
     Route::get('/showStatement/{id}', 'StatementController@showStatement');
     Route::delete('/deleteParameter/{id}', 'ParameterController@deleteParameter');
