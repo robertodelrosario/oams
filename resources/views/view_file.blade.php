d8e9f5a4193668a1c68376b5476e841757c3c09d
roberto del rosario <roberto.delrosario@ustp.edu.ph>
2021-04-20 14:43:00 +0800

    updated

--- resources/views/view_file.blade.php
+++ resources/views/view_file.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <body>
 <p>
-    <iframe src="{{ '/var/www/html/oams/storage/app/app/'.$file_link->link }}" style="width: 100%; height: 1000px"> </iframe>
+    <iframe src="{{ 'http://202.137.117.169/core/var/www/html/oams/storage/app/app/'.$file_link->link }}" style="width: 100%; height: 1000px"> </iframe>
 </p>
 </body>
 
