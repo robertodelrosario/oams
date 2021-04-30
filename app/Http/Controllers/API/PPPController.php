741f8e6c9c933b28897161ece56a976902407e64
roberto del rosario <roberto.delrosario@ustp.edu.ph>
2021-04-30 11:18:09 +0800

    updated

--- app/Http/Controllers/API/PPPController.php
+++ app/Http/Controllers/API/PPPController.php
@@ -21,6 +21,7 @@ use Illuminate\Support\Collection;
 require_once '/var/www/html/oams/vendor/autoload.php';
 //require_once 'C:\laragon\www\online_accreditation_management_system\vendor/autoload.php';
 //use  \PhpOffice\PhpWord\PhpWord;
+use PhpOffice\PhpWord\PhpWord;
 
 class PPPController extends Controller
 {
@@ -202,7 +203,7 @@ class PPPController extends Controller
 //            }
 //        }
 
-        $phpWord = new \PhpOffice\PhpWord\PhpWord();
+        $phpWord = new PhpWord();
         $section = $phpWord->addSection();
 
         $styleFont1 = array('align'=>\PhpOffice\PhpWord\Style\Cell::VALIGN_CENTER);
