3bc5cdff312d509a99ae04da40942712955b4661
roberto del rosario <roberto.delrosario@ustp.edu.ph>
2021-04-30 11:15:35 +0800

    updated

--- app/Http/Controllers/API/PPPController.php
+++ app/Http/Controllers/API/PPPController.php
@@ -20,7 +20,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 require_once '/var/www/html/oams/vendor/autoload.php';
 //require_once 'C:\laragon\www\online_accreditation_management_system\vendor/autoload.php';
-use  \PhpOffice\PhpWord\PhpWord;
+//use  \PhpOffice\PhpWord\PhpWord;
 
 class PPPController extends Controller
 {
