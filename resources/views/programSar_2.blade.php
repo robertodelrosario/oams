486a3221130be47994f20d18e12d9f1017d24f32
roberto del rosario <roberto.delrosario@ustp.edu.ph>
2021-06-09 14:45:46 +0800

    updated

--- resources/views/programSar_2.blade.php
+++ resources/views/programSar_2.blade.php
@@ -100,8 +100,8 @@
     </table>
     <table class="table-borderless" >
         <tr>
-            <td  class="small" style="text-align: left; font-size: 12px; width: 512px">Award {{ $level }}</td>
-            <td class="small" style="text-align: right; font-size: 12px; width: 256px">______________</td>
+            <td  class="small" style="text-align: left; font-size: 12px; width: 70%">Award {{ $level }}</td>
+            <td class="small" style="text-align: right; font-size: 12px; width: 30%">______________</td>
         </tr>
     </table>
     <table class="table-borderless" >
