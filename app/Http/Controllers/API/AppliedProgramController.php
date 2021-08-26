890fe032c67cfc2e26ab03e29b3615f6fd550b65
roberto del rosario <roberto.delrosario@ustp.edu.ph>
2021-08-26 15:38:40 +0800

    updated

--- app/Http/Controllers/API/AppliedProgramController.php
+++ app/Http/Controllers/API/AppliedProgramController.php
@@ -323,13 +323,16 @@ class AppliedProgramController extends Controller
         $newfile = ApplicationProgramFile::where('id', $id)->first();
         $user = User::where('id', $newfile->uploader_id)->first();
         if($newfile->uploader_id != auth()->user()->id) return response()->json(['status' => false, 'message' => 'Only '.$user->first_name." ".$user->last_name." can update the file."]);
-        $fileName = $request->file->getClientOriginalName();
-        $filePath = $request->file->storeAs('application/files', $fileName);
-        $newfile->file_title = $fileName;
-        $newfile->file = $filePath;
-        $success = $newfile->save();
-        if ($success) return response()->json(['status' => true, 'message' => 'Successfully updated file']);
-        else return response()->json(['status' => false, 'message' => 'Unsuccessfully updated file']);
+        if(!(is_null($request->file))) {
+            $fileName = $request->file->getClientOriginalName();
+            $filePath = $request->file->storeAs('application/files', $fileName);
+            $newfile->file_title = $fileName;
+            $newfile->file = $filePath;
+            $success = $newfile->save();
+            if ($success) return response()->json(['status' => true, 'message' => 'Successfully updated file']);
+            else return response()->json(['status' => false, 'message' => 'Unsuccessfully updated file']);
+        }
+        return response()->json(['status' => false, 'message' => 'null']);
     }
 
     public function deleteProgramFile($id, $user_id){
