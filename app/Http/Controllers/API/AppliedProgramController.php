2339c04e6294766166f580f0295a3788a629a8b3
roberto del rosario <roberto.delrosario@ustp.edu.ph>
2021-08-26 15:53:29 +0800

    updated

--- app/Http/Controllers/API/AppliedProgramController.php
+++ app/Http/Controllers/API/AppliedProgramController.php
@@ -323,7 +323,7 @@ class AppliedProgramController extends Controller
         $newfile = ApplicationProgramFile::where('id', $id)->first();
         $user = User::where('id', $newfile->uploader_id)->first();
         if($newfile->uploader_id != auth()->user()->id) return response()->json(['status' => false, 'message' => 'Only '.$user->first_name." ".$user->last_name." can update the file."]);
-        if(!(is_null($request->filename))) {
+        if(!(is_null($request->file('filename')))) {
             $fileName = $request->filename->getClientOriginalName();
             $filePath = $request->filename->storeAs('application/files', $fileName);
             $newfile->file_title = $fileName;
