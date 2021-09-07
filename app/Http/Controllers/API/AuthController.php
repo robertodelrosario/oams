945626a88a1a94d62ea50f58d4b91664a8ca605b
roberto del rosario <roberto.delrosario@ustp.edu.ph>
2021-09-07 15:23:07 +0800

    updated

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -511,7 +511,6 @@ class AuthController extends Controller
                 foreach ($collection_1 as $col){
                     if($col['role_id'] == 1 || $col['role_id'] == 2){
                         $sub_offices = Office::where('parent_office_id', $head_office_id)->get();
-                        dd($head_office_id);
                         foreach ($sub_offices as $sub_office){
                             if($sub_office->id == $col['office_id']){
                                 $collection_head->push([
