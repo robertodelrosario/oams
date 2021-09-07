090dc74ca63550eeff89fd5eaaa1fc0da055ef01
roberto del rosario <roberto.delrosario@ustp.edu.ph>
2021-09-07 15:31:47 +0800

    updated

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -418,10 +418,9 @@ class AuthController extends Controller
         $is_active_chairman = false;
         $chairman_office_id = 0;
         $head_office_id = 0;
-        echo $user_roles;
         foreach ($user_roles as $user_role){
             if($user_role->role_id == 5 || $user_role->role_id == 6) $is_active_qa = true;
-            if($user_role->role_id = 2) {
+            elseif($user_role->role_id = 2) {
                 $is_active_chairman = true;
                 $user_offices = OfficeUser::where('user_role_id', $user_role->id)->get();
                 foreach ($user_offices as $user_office){
@@ -429,7 +428,7 @@ class AuthController extends Controller
                     $chairman_office_id = $chairman_office->id;
                 }
             }
-            if($user_role->role_id = 11) {
+            elseif($user_role->role_id = 11){
                 $is_active_head = true;
                 $user_offices = OfficeUser::where('user_role_id', $user_role->id)->get();
                 foreach ($user_offices as $user_office){
