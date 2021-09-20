b114960c1722b92785e705215f9b927cf63de6b7
roberto del rosario <roberto.delrosario@ustp.edu.ph>
2021-09-20 14:37:10 +0800

    updated

--- app/Http/Controllers/API/ReportController.php
+++ app/Http/Controllers/API/ReportController.php
@@ -6,10 +6,15 @@ use App\ApplicationProgram;
 use App\AreaInstrument;
 use App\AreaMean;
 use App\AssignedUser;
+use App\BenchmarkStatement;
 use App\Campus;
 use App\Http\Controllers\Controller;
 use App\InstrumentProgram;
+use App\InstrumentScore;
+use App\ParameterProgram;
 use App\Program;
+use App\ProgramStatement;
+use App\Recommendation;
 use App\SFRInformation;
 use App\SUC;
 use App\User;
@@ -498,6 +503,165 @@ class ReportController extends Controller
             return $pdf->download($program->program_name . '_SAR.pdf');
             return response()->json(['program' => $program, 'areas' => $program_sar, 'result' => $result]);
         }
+    }
+
+    public function generateAccreditorReport($id){
+        $applied_program = ApplicationProgram::where('id', $id)->first();
+        $program = Program::where('id', $applied_program->program_id)->first();
+        $area = AssignedUser::where([
+            ['app_program_id', $id], ['user_id', auth()->user()->id]
+        ])->first();
+        $campus = Campus::where('id', $program->campus_id)->first();
+        $suc = SUC::where('id', $campus->suc_id)->first();
+        $accreditor = User::where('id', auth()->user()->id)->first();
+        if (Str::contains($area->role, 'external accreditor')) $role = 'external accreditor';
+        else $role = 'internal accreditor';
+        $instruments = new Collection();
+        $program_instruments = InstrumentProgram::where('program_id', $program->id)->get();
+        foreach ($program_instruments as $program_instrument){
+            $area_instrument = AreaInstrument::where('id', $program_instrument->area_instrument_id)->first();
+            $instruments->push([
+                'id' => $program_instrument->id,
+                'program_id' => $program_instrument->program_id,
+                'area_instrument_id' => $program_instrument->area_instrument_id,
+                'program_name' => $program->program_name,
+                'intended_program_id' => $area_instrument->intended_program_id,
+                'area_number' => $area_instrument->area_number,
+                'area_name' => $area_instrument->area_name,
+            ]);
+        }
+
+        $assigned_users = AssignedUser::where([
+            ['app_program_id', $id], ['role', 'like', '%'. $role.'%']
+        ])->get();
+        $scores = new Collection();
+        $accreditor_area_mean_score = new Collection();
+        $accreditor_total_score = new Collection();
+        $recommendation_collection = new Collection();
+
+        foreach ($assigned_users as $assigned_user){
+            $recommendations = Recommendation::where('assigned_user_id', $assigned_user->id)->get();
+            foreach ($recommendations as $recommendation){
+                $recommendation_collection->push([
+                    'instrument_id' => $assigned_user->transaction_id,
+                    'recommendation' => $recommendation->recommendation
+                ]);
+            }
+        }
+
+        foreach ($instruments as $instrument){
+            $parameter = ParameterProgram::where('program_instrument_id', $instrument['id'])->first();
+            $statements = ProgramStatement::where('program_parameter_id', $parameter->id)->get();
+            $statement_scores = new Collection();
+            $collection_id = new Collection();
+            $collection_statements = new Collection();
+            foreach ($statements as $statement){
+                $benchmark_statement = BenchmarkStatement::where('id', $statement->benchmark_statement_id)->first();
+                if(!($collection_id->contains($statement->id))){
+                    $collection_id->push(['id' => $statement->id]);
+                    $collection_statements->push([
+                        'id' => $statement->id,
+                        'instrument_parameter_id' => $statement->instrument_parameter_id,
+                        'benchmark_statement_id' => $statement->benchmark_statement_id,
+                        'parent_statement_id' => $statement->parent_statement_id,
+                        'benchmark_statement' => $benchmark_statement->statement,
+                        'degree' => " "
+                    ]);
+
+                    foreach ($statements as $statement_1){
+                        if($statement->id == $statement_1->parent_statement_id){
+                            $collection_id->push(['id' => $statement_1->id]);
+                            $benchmark_statement_1 = BenchmarkStatement::where('id', $statement_1->benchmark_statement_id)->first();
+                            $collection_statements->push([
+                                'id' => $statement_1->id,
+                                'instrument_parameter_id' => $statement_1->instrument_parameter_id,
+                                'benchmark_statement_id' => $statement_1->benchmark_statement_id,
+                                'parent_statement_id' => $statement_1->parent_statement_id,
+                                'benchmark_statement' => $benchmark_statement_1->statement,
+                                'degree' => "  "
+                            ]);
+
+                            foreach ($statements as $statement_2){
+                                if($statement_1->id == $statement_2->parent_statement_id){
+                                    $collection_id->push(['id' => $statement_1->id]);
+                                    $benchmark_statement_2 = BenchmarkStatement::where('id', $statement_2->benchmark_statement_id)->first();
+                                    $collection_statements->push([
+                                        'id' => $statement_2->id,
+                                        'instrument_parameter_id' => $statement_2->instrument_parameter_id,
+                                        'benchmark_statement_id' => $statement_2->benchmark_statement_id,
+                                        'parent_statement_id' => $statement_2->parent_statement_id,
+                                        'benchmark_statement' => $benchmark_statement_2->statement,
+                                        'degree' => "   "
+                                    ]);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            $area_mean = 0;
+            $count = 0;
+            foreach ($assigned_users as $assigned_user){
+                $available = 0;
+                $inadequate = 0;
+                if($instrument['id'] == $assigned_user->transaction_id){
+                    $user = User::where('id', $assigned_user->user_id)->first();
+                    foreach ($statements as $statement){
+                        $statement_score = InstrumentScore::where([
+                            ['item_id', $statement->id], ['assigned_user_id', $assigned_user->id]
+                            ])->first();
+                        $statement_scores->push([
+                            'item_id' =>  $statement->id,
+                            'assigned_user_id' => $assigned_user->id,
+                            'user_id' => $assigned_user->user_id,
+                            'score' => $statement_score->item_score
+                        ]);
+                        if($statement_score->item_score >= 3) $available = $available + $statement_score->item_score;
+                        elseif($statement_score->item_score >= 1) $inadequate = $inadequate + $statement_score->item_score;
+                    }
+                    $accreditor_total_score->push([
+                        'instrument_id' => $instrument['id'],
+                        'user_id' => $user->id,
+                        'last_name'=> $user->last_name,
+                        'available' => $available,
+                        'inadequate' => $inadequate,
+                    ]);
+                }
+                $accreditor_area_mean = AreaMean::where([
+                    ['instrument_program_id', $instrument['id']], ['assigned_user_id',$assigned_user->id]
+                ])->first();
+                if(!(is_null($accreditor_area_mean))) {
+                    $area_mean = $area_mean + $accreditor_area_mean->area_mean;
+                    $count++;
+                }
+            }
+            if($count != 0) $area_mean = $area_mean/$count;
+            $accreditor_area_mean_score->push([
+                'instrument_program_id' => $instrument['id'],
+                'area_mean' => $area_mean
+            ]);
+            foreach ($collection_statements as $collection_statement){
+                $user_score = new Collection();
+                foreach ($statement_scores as $ss){
+                    if($collection_statement['id'] == $ss['item_id']){
+                        $user = User::where('id', $ss['user_id'])->first();
+                        $user_score->push([
+                            'last_name' => $user->last_name,
+                            'score' => $ss->score
+                        ]);
+                    }
+                }
+                $scores->push([
+                    'id' => $instrument['id'],
+                    'statement' => $collection_statement['benchmark_statement'],
+                    'degree' => $collection_statement['degree'],
+                    'score' => $user_score
+                ]);
+            }
+        }
+
+        $pdf = PDF::loadView('accreditor_report', ['program' => $program,'campus' => $campus, 'suc'=>$suc, 'accreditor' => $accreditor ,'areas' => $instruments, 'result' => $scores, 'recommendations' => $recommendation_collection, 'grand_mean'=> $accreditor_area_mean_score]);
+        return $pdf->download($program->program_name . '_ACCREDITOR_REPORT.pdf');
     }
 
     public function generateProgramSFR($id, $role){
---
+++ resources/views/accreditor_report.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Preliminary Survey Visit</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+    <h3 style="text-align: center">Preliminary Survey Visit</h3>
+    <table class="table-borderless" >
+        <tr>
+            <th scope="col" class="font-weight-bold" style="text-align: right; font-size: 14px; width: 90%">Program: {{ $program->program_name }}</th>
+        </tr>
+    </table>
+    <table class="table-borderless" >
+        <tr>
+            <th scope="col" class="font-weight-bold" style="text-align: right; font-size: 14px; width: 90%">SUC/Campus: {{ $suc->institution_name }} / {{ $campus->campus_name }} </th>
+        </tr>
+    </table>
+    <table class="table-borderless" >
+        <tr>
+            <th scope="col" class="font-weight-bold" style="text-align: right; font-size: 14px; width: 90%">Address: {{ $campus->address}}</th>
+        </tr>
+    </table>
+    <table class="table-borderless" >
+        <tr>
+            <th scope="col" class="font-weight-bold" style="text-align: right; font-size: 14px; width: 90%">Accreditor: {{ $accreditor->first_name }} {{ $accreditor->last_name }}</th>
+        </tr>
+    </table>
+    <div style="page-break-after: always"></div>
+    @foreach($areas as $area)
+        <h4 style="text-align: center">{{ $area['area_name'] }}</h4>
+        <table class="table table-bordered" >
+            <thead>
+            <tr>
+                <th scope="col" class="font-weight-bold" style="text-align: center; font-size: 12px; width: 90%">Checklist of data/information, processes and activities</th>
+                <th scope="col" class="font-weight-bold" style="text-align: center; font-size: 12px; width: 30%">Available</th>
+                <th scope="col" class="font-weight-bold" style="text-align: center; font-size: 12px; width: 30%">Available but Inadequate</th>
+                <th scope="col" class="font-weight-bold" style="text-align: center; font-size: 12px; width: 30%">Not Available</th>
+                <th scope="col" class="font-weight-bold" style="text-align: center; font-size: 12px; width: 30%">Not Applicable</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($scores as $score)
+                <tr>
+                    <th scope="row" class="small">{{$score['degree']}}{{ $score['statement'] }}</th>
+                    <td class="small" >
+                        @foreach($score['score'] as $user_score)
+                            @if($user_score['score'] >= 3 && $user_score['score'] <= 5)
+                                {{ $user_score['last_name'] }} : {{ $user_score['score'] }}
+                            @endif
+                        @endforeach
+                    </td>
+                    <td class="small">
+                        @foreach($score['score'] as $user_score)
+                            @if($user_score['score'] == 1 || $user_score['score'] == 2)
+                                {{ $user_score['last_name'] }} : {{ $user_score['score'] }}
+                            @endif
+                        @endforeach
+                    </td>
+                    <td class="small">
+                        @foreach($score['score'] as $user_score)
+                            @if($user_score['score'] == 0)
+                                {{ $user_score['last_name'] }} : {{ $user_score['score'] }}
+                            @endif
+                        @endforeach
+                    </td>
+                    <td class="small">
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    @endforeach
+</div>
+
+</body>
+</html>
