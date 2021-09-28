253e77e7c9153ef9992990f4861d82bbbe6348b9
roberto del rosario <roberto.delrosario@ustp.edu.ph>
2021-09-28 09:25:26 +0800

    updated

--- resources/views/accreditor_report.blade.php
+++ resources/views/accreditor_report.blade.php
@@ -33,6 +33,23 @@
             <th scope="col" class="font-weight-bold" style="text-align: right; font-size: 14px; width: 90%">Accreditor: {{ $accreditor['first_name'] }} {{ $accreditor['last_name'] }}</th>
         </tr>
     </table>
+    @foreach($areas as $area)
+        <div style="page-break-after: always"></div>
+        <h4 style="text-align: center">{{ $area['area_name'] }}</h4>
+        <table class="table table-bordered" >
+            <thead>
+            <tr>
+                <th scope="col" class="font-weight-bold" style="text-align: center; font-size: 12px; width: 90%">Checklist of data/information, processes and activities</th>
+                <th scope="col" class="font-weight-bold" style="text-align: center; font-size: 12px; width: 30%">Available</th>
+                <th scope="col" class="font-weight-bold" style="text-align: center; font-size: 12px; width: 30%">Available but Inadequate</th>
+                <th scope="col" class="font-weight-bold" style="text-align: center; font-size: 12px; width: 30%">Not Available</th>
+                <th scope="col" class="font-weight-bold" style="text-align: center; font-size: 12px; width: 30%">Not Applicable</th>
+            </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+    @endforeach
 </div>
 
 </body>
