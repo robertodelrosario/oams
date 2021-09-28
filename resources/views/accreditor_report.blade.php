d82c6f8f3d2a5e1b31b1a8301b31f88333485fe5
roberto del rosario <roberto.delrosario@ustp.edu.ph>
2021-09-28 11:04:01 +0800

    updated

--- resources/views/accreditor_report.blade.php
+++ resources/views/accreditor_report.blade.php
@@ -49,32 +49,7 @@
             <tbody>
             @foreach($result as $score)
                 @if($score['id'] == $area['id'])
-                    <tr>
-                        <th scope="row" class="small">{{ $score['degree'] }} {{ $score['statement'] }}</th>
-                        <td class="small" >
-                            @foreach($score['score'] as $user_score)
-                                @if($user_score['score'] >= 3 && $user_score['score'] <= 5)
-                                    {{ $user_score['last_name'] }} : {{ $user_score['score'] }}
-                                @endif
-                            @endforeach
-                        </td>
-                        <td class="small">
-                            @foreach($score['score'] as $user_score)
-                                @if($user_score['score'] == 1 || $user_score['score'] == 2)
-                                    {{ $user_score['last_name'] }} : {{ $user_score['score'] }}
-                                @endif
-                            @endforeach
-                        </td>
-                        <td class="small">
-                            @foreach($score['score'] as $user_score)
-                                @if($user_score['score'] == 0)
-                                    {{ $user_score['last_name'] }} : {{ $user_score['score'] }}
-                                @endif
-                            @endforeach
-                        </td>
-                        <td class="small">
-                        </td>
-                    </tr>
+
                 @endif
             @endforeach
             </tbody>
