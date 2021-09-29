5bf27eb8a2aef6427098ebee3df1a4f7ad8615f2
roberto del rosario <roberto.delrosario@ustp.edu.ph>
2021-09-29 13:45:39 +0800

    updated

--- app/Http/Controllers/API/ReportController.php
+++ app/Http/Controllers/API/ReportController.php
@@ -706,7 +706,7 @@ class ReportController extends Controller
                 ]);
 
                 foreach ($statements as $statement_1) {
-                    if ($statement->benchmark_statement_id == $statement_1->parent_statement_id) {
+                    if ($statement->benchmark_statement_id == $statement_1->parent_statement_id && !($collection_id->contains($statement_1->id))) {
                         $collection_id->push(['id' => $statement_1->id]);
                         $benchmark_statement_1 = BenchmarkStatement::where('id', $statement_1->benchmark_statement_id)->first();
                         $collection_statements->push([
@@ -719,7 +719,7 @@ class ReportController extends Controller
                         ]);
 
                         foreach ($statements as $statement_2) {
-                            if ($statement_1->benchmark_statement_id == $statement_2->parent_statement_id) {
+                            if ($statement_1->benchmark_statement_id == $statement_2->parent_statement_id && !($collection_id->contains($statement_2->id))) {
                                 $collection_id->push(['id' => $statement_2->id]);
                                 $benchmark_statement_2 = BenchmarkStatement::where('id', $statement_2->benchmark_statement_id)->first();
                                 $collection_statements->push([
