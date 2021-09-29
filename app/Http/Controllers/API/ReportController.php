8fd9a76238a447855d61450d2924d79c82b3f672
roberto del rosario <roberto.delrosario@ustp.edu.ph>
2021-09-29 10:03:32 +0800

    updated

--- app/Http/Controllers/API/ReportController.php
+++ app/Http/Controllers/API/ReportController.php
@@ -776,8 +776,8 @@ class ReportController extends Controller
             $accreditor_area_mean = AreaMean::where([
                 ['instrument_program_id', $program_instrument->id], ['assigned_user_id',$assigned_user->id]
             ])->first();
-            echo $accreditor_area_mean->area_mean;
             if(!(is_null($accreditor_area_mean))) {
+                dd($accreditor_area_mean->area_mean);
                 $area_mean = $area_mean + $accreditor_area_mean->area_mean;
                 $count++;
             }
