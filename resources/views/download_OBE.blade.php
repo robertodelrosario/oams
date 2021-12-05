ab7219677b0cea56f67cfb6bdb7d0a2364e611f6
roberto del rosario <roberto.delrosario@ustp.edu.ph>
2021-12-06 00:30:35 +0800

    updated

--- resources/views/download_OBE.blade.php
+++ resources/views/download_OBE.blade.php
@@ -98,7 +98,7 @@
             <tr>
                 <th scope="col" class="font-weight-bold" style="text-align: center; font-size: 12px; width: 55%;">Indicators</th>
                 <th scope="col" class="font-weight-bold" style="text-align: center; font-size: 12px; width: 10%;writing-mode: vertical-lr"">Item Rating (IR)</th>
-                <th scope="col" class="font-weight-bold" style="text-align: center; font-size: 12px; width: 20%; -webkit-writing-mode: vertical-lr"">System-Implementation-outcome Mean (SIOM)</th>
+                <th scope="col" class="font-weight-bold" style="text-align: center; font-size: 12px; width: 20%; -webkit-writing-mode: sideways-lr"">System-Implementation-outcome Mean (SIOM)</th>
                 <th scope="col" class="font-weight-bold" style="text-align: center; font-size: 12px; width: 15%;writing-mode: vertical-lr"">Parameter Mean (PM)</th>
             </tr>
         </thead>
