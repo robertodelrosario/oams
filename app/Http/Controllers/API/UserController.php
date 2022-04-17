8b54232de91324d1047bea85cff9b416a84a808c
rdelrosariojr <roberto.delrosario@sun-asterisk.com>
2022-04-17 21:26:09 +0800

    mofified notification

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -161,12 +161,9 @@ class UserController extends Controller
         $weight = array(0,8,8,8,5,4,5,3,4,5);
 
         $sar_external = new Collection();
-        $x = 0;
         foreach ($area_mean_external as $area){
-            $x+1;
-            if($x < 11)
-            {try {
-                echo $area->instrument_program_id;
+            try {
+                echo count($area_mean_external);
                 if($area->instrument_program_id != null)
                 {
                     $instrument = InstrumentProgram::where('id', $area->instrument_program_id)->first();
@@ -180,7 +177,7 @@ class UserController extends Controller
                 }   
               } catch (Exception $e) {
                       echo "error";
-              }}
+              }
             
         }
 
