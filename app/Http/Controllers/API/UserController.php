b4041a2479846bcb7796d314a2a72590ed98c310
rdelrosariojr <roberto.delrosario@sun-asterisk.com>
2022-04-17 21:01:10 +0800

    mofified notification

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -163,7 +163,7 @@ class UserController extends Controller
         $sar_external = new Collection();
         foreach ($area_mean_external as $area){
             $instrument = InstrumentProgram::where('id', $area->instrument_program_id)->first();
-            if ($instrument){$area_number = AreaInstrument::where('id', $instrument->area_instrument_id)->first();
+            if (!$instrument){$area_number = AreaInstrument::where('id', $instrument->area_instrument_id)->first();
             for($x=0;$x < 10; $x++){
                 if($area_number->area_number == $x+1){
                     $sar_external->push(['instrument_program_id' => $instrument->id,'area_number' => $area_number->area_number,'area' => $area_number->area_name, 'weight' => $weight[$x], 'area_mean' => round($area->area_mean, 2), 'weighted_mean' => round($area->area_mean * $weight[$x], 2)]);
